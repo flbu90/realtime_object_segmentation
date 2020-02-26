841f2275f018307fdff871e2cf0dd2c3c4763a32
flbu90 <flbusch@web.de>
2020-02-26 13:35:02 +0100

    Update and rename tfRun_Trained_CNN.py to rs_test_model.py

--- rs_test_model.py
+++ rs_test_model.py
@@ -11,17 +11,14 @@ import scipy.io as io
 
 # -------------------------------------------- Display the model.checkpoint -----------------------------------------
 
-
 # import the inspect_checkpoint library
 from tensorflow.python.tools import inspect_checkpoint as chkp
 
 # print all tensors in checkpoint file
 chkp.print_tensors_in_checkpoint_file("3DCNN_Model/model.checkpoint", tensor_name='', all_tensors=True)
 
-
-
 # ---------------------------------------------- Display the Image ----------------------------------------------------
-'''
+
 test_voxels = np.load("Full_Testdata/cup/30/test/cup_000000315_1.npy")
 
 test_voxels = np.pad(test_voxels,(1,1), 'constant', constant_values=(0,0)) # aus 30x30x30 wird 32x32x32
@@ -33,5 +30,3 @@ bx = fig.gca(projection='3d')
 bx.set_aspect('equal')
 bx.voxels(test_voxels, edgecolor='red')
 plt.show(test_voxels)
-
-'''
